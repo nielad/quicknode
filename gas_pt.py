afd8188f49b037fb9a3607aebfb0ba7b20f0d40a
nielad <nielad@gmail.com>
2022-11-06 08:58:12 -0800

    Update gas_pt.py

--- gas_pt.py
+++ gas_pt.py
@@ -1,17 +1,13 @@
 from web3 import Web3, HTTPProvider
 import statistics
+
+x = 'x'
 web3 = Web3(HTTPProvider("https://dawn-distinguished-sound.ethereum-goerli.discover.quiknode.pro/bd15ae9cf37d801f052837a2bfe6b330752fd949/"))
 pending_transactions = web3.provider.make_request("parity_pendingTransactions", [])
 gas_prices = []
 gases = []
-for tx in pending_transactions["result"[:10]]:
-	gas_prices.append(int((tx["gasPrice"]),16))
-	gases.append(int((tx["gas"]),16))
-
-print("Average:")
-print("-"*80)
-print("gasPrice: ", statistics.mean(gas_prices))
-print(" ")
-print("Median:")
-print("-"*80)
-print("gasPrice: ", statistics.median(gas_prices))
+for tx in pending_transactions:
+	gas_prices.append(int(tx, 16))
+	gases.append(int(tx,16))
+gmean = statistics.mean(gas_prices)
+gmedian = statistics.mean(gas_prices)
